52075e1d38436d52d6c43e5301095572702b017e
Antoshidza <antoshidza@mail.ru>
2022-11-19 11:28:15 +0300

    Update SquadAuthoring.cs
    Fix SquadAuthoring doesn't add PrevWorldPosition2D

--- Assets/Sources/Rome/Authorings/SquadAuthoring.cs
+++ Assets/Sources/Rome/Authorings/SquadAuthoring.cs
@@ -10,7 +10,9 @@ public class SquadAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        _ = dstManager.AddComponentData(entity, new WorldPosition2D { value = new float2(transform.position.x, transform.position.y) });
+        var pos = new float2(transform.position.x, transform.position.y);
+        _ = dstManager.AddComponentData(entity, new WorldPosition2D { value = pos });
+        _ = dstManager.AddComponentData(entity, new PrevWorldPosition2D { value = pos });
         _ = dstManager.AddComponentData(entity, new SquadSettings { squadResolution = _resolution, soldierMargin = _soldierMargin });
         _ = dstManager.AddComponentData(entity, new RequireSoldier { count = _resolution.x * _resolution.y });
         _ = dstManager.AddBuffer<SoldierLink>(entity);
