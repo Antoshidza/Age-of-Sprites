06a2a619d6003c6612c011e445c89f8421d80881
Antoshidza <antoshidza@mail.ru>
2022-11-29 14:11:25 +0300

    Remove test sources

--- Assets/Sources/Test/Debug/MoveAround.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-public struct MoveAround : IComponentData
-{
-    public float2 startPosition;
-    public float2 area;
-    public float timeOffset;
-}
--- Assets/Sources/Test/Debug/MoveAroundScreen.cs
+++
@@ -1,8 +0,0 @@
-﻿using Unity.Entities;
-using Unity.Mathematics;
-
-[GenerateAuthoringComponent]
-public struct MoveAroundScreen : IComponentData
-{
-    public float2 destination;
-}
