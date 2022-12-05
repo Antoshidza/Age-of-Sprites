338c3248106e307eeaf64f743991ab629740de17
Antoshidza <antoshidza@mail.ru>
2022-12-05 15:38:07 +0300

    Add camera comtrol
    Camera control works same way with mouse and touchscreen devices

---
+++ Assets/Sources/Rome/Common/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float2 _sizeMinMax;
+    [SerializeField] private float _zoomSpeed = 1f;
+    [SerializeField] private float _moveSpeed = 1f;
+    private float _targetSize;
+    private CameraControl _cameraControl;
+
+    private const float DeltaSizeThreshold = 0.01f;
+
+    private void Start()
+    {
+        _cameraControl = new();
+        _cameraControl.Enable(); // enable all actions
+        _targetSize = _camera.orthographicSize;
+        _cameraControl.Camera.Zoom.performed += (context) =>
+        {
+            var zoom = context.ReadValue<float>();
+
+            // adjust target size depending on zoom value if zoom value has same direction as previous
+            // otherwise turn target size to opposite direction
+            _targetSize = math.clamp((math.sign(zoom) == math.sign(_targetSize) ? _targetSize : _camera.orthographicSize) + zoom, _sizeMinMax.x, _sizeMinMax.y);
+        };
+        _cameraControl.Camera.Move.performed += (context) =>
+        {
+            var moveOffset = context.ReadValue<Vector2>() * _moveSpeed;
+            _camera.transform.Translate(moveOffset.x, moveOffset.y, 0f);
+        };
+    }
+    private void Update()
+    {
+        var deltaSize = math.abs(_camera.orthographicSize - _targetSize);
+        if(deltaSize > DeltaSizeThreshold)
+            _camera.orthographicSize = math.lerp(_camera.orthographicSize, _targetSize, math.saturate(Time.deltaTime * _zoomSpeed / deltaSize));
+    }
+    private void OnEnable()
+    {
+        _cameraControl?.Enable();
+    }
+    private void OnDisable()
+    {
+        _cameraControl?.Disable();
+    }
+}
---
+++ Assets/Sources/Rome/Input/PinchingComposite.cs
@@ -0,0 +1,54 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Utilities;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
+
+[InitializeOnLoad]
+#endif
+[DisplayStringFormat("{firstTouch}+{secondTouch}")]
+public class PinchingComposite : InputBindingComposite<float>
+{
+    [InputControl(layout = "Value")]
+    public int firstTouch;
+    [InputControl(layout = "Value")]
+    public int secondTouch;
+
+    public float negativeScale = 1f;
+    public float positiveScale = 1f;
+
+    private struct TouchStateComparer : IComparer<TouchState>
+    {
+        public int Compare(TouchState x, TouchState y) => 1;
+    }
+
+    // This method computes the resulting input value of the composite based
+    // on the input from its part bindings.
+    public override float ReadValue(ref InputBindingCompositeContext context)
+    {
+        var touch_0 = context.ReadValue<TouchState, TouchStateComparer>(firstTouch);
+        var touch_1 = context.ReadValue<TouchState, TouchStateComparer>(secondTouch);
+
+        if (touch_0.phase != TouchPhase.Moved || touch_1.phase != TouchPhase.Moved)
+            return 0f;
+
+        var startDistance = math.distance(touch_0.startPosition, touch_1.startPosition);
+        var distance = math.distance(touch_0.position, touch_1.position);
+
+        var unscaledValue = startDistance / distance - 1f; // startDistance divide by distance to invert value
+        return unscaledValue * (unscaledValue < 0 ? negativeScale : positiveScale);
+    }
+
+    // This method computes the current actuation of the binding as a whole.
+    public override float EvaluateMagnitude(ref InputBindingCompositeContext context) => 1f;
+
+    static PinchingComposite() => InputSystem.RegisterBindingComposite<PinchingComposite>();
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void Init() { } // Trigger static constructor.
+}
