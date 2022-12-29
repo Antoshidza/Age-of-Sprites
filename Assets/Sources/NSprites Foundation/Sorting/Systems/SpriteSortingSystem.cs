6b534d8b828ffd0b0bd2218c8253e1034ea28328
TonyMax <antoshidza@mail.ru>
2022-12-29 17:47:31 +0300

    Update SpriteSortingSystem.cs

--- Assets/Sources/NSprites Foundation/Sorting/Systems/SpriteSortingSystem.cs
+++ Assets/Sources/NSprites Foundation/Sorting/Systems/SpriteSortingSystem.cs
@@ -6,6 +6,9 @@ using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
+using static NSprites.SpriteSortingSystem;
+
+[assembly: RegisterGenericJobType(typeof(SortArrayJob<int, SortingDataComparer>))]
 
 namespace NSprites
 {
