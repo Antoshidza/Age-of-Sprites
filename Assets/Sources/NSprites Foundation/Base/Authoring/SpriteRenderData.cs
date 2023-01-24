000ec283080291ad9fc928816cbb1902a846fe00
TonyMax <antoshidza@mail.ru>
2023-01-24 13:12:39 +0300

    * Update unity version to v2022.2.3f1 * Remove workaround from SpriteRenderData.cs to fix wrong serialization in build
    Commit completely fixes issue https://github.com/Antoshidza/Age-of-Sprites/issues/6

--- Assets/Sources/NSprites Foundation/Base/Authoring/SpriteRenderData.cs
+++ Assets/Sources/NSprites Foundation/Base/Authoring/SpriteRenderData.cs
@@ -9,7 +9,5 @@ namespace NSprites
         public int ID => Material.GetHashCode();
         public Material Material;
         public PropertiesSet PropertiesSet;
-
-        [SerializeField] private SpriteAnimation fixNullRefSO_1; // TODO: remove it after fixing https://github.com/Antoshidza/Age-of-Sprites/issues/6
     }
 }
